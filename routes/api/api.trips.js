35daa3a0020aa427436370bc3922086fb76d5c72
Yu-Ching Huang <yu.ching.huang@outlook.com>
2025-04-11 14:17:57 -0400

    edit post route in api.trip.js

--- routes/api/api.trips.js
+++ routes/api/api.trips.js
@@ -14,14 +14,14 @@ const TripService = imageController.TripImageService;
 
 //read all trips (http://localhost:8086/api/trips)
 router.get('/', async(req, res, next)=>{
-    console.log('-----------In apiRouter--------------')
+    console.log('-----------In apiRouter--------------');
     try{
         const trips = await TripService.list()
-        console.log(`API: List of trip contents and trip images ${trips}`)
+        console.log(`API: List of trip contents and trip images ${trips}`);
         res.status(200);
-        res.json(trips)
+        res.json(trips);
     } catch (err){
-        console.error(`Error in retrieving trips from MongoDB, ${err}`)
+        console.error(`Error in retrieving trips from MongoDB, ${err}`);
         res.status(404);
         res.end();
     }
@@ -30,7 +30,34 @@ router.get('/', async(req, res, next)=>{
 //read a single trip
 
 //create a single trip
+router.post('/', async(req, res, next) => {
+    const path ='/static/images/' + req.file.filename
+    const tripData = {
+    originalname: req.file.originalname,
+    mimetype: req.file.mimetype,
+    filename: req.file.filename,
+    imageurl: path,
+    //convert file size into KB
+    size: req.file.size / 1024 |0, 
+    username: req.body.username,
+    title: req.body.title,
+    country: req.body.country,
+    city: req.body.city,
+    content: req.body.content
+    }
 
+    try {
+        const trip = await TripService.create(tripData);
+        res.send(200);
+        console.log('-----Create a trip json format-------')
+        res.json(trip);
+    } catch (err){
+        console.error(`Error in saving a new trip itinerary: ${err}`);
+        res.status(404);
+        res.end();
+    }
+    
+});
 //update a single trip
 
 //delete a single trip

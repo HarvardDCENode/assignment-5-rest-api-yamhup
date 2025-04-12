da08094f78ae4060fac26d4fdb1e706b41effafa
Yu-Ching Huang <yu.ching.huang@outlook.com>
2025-04-12 18:30:28 -0400

    add callAPI GET method

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,41 @@
 
-    (function(){
-        const URL  = 'localhost:8086';
-    
-        document.querySelector('#test-button').addEventListener("click", ()=> {
-            console.log("Test API button been clicked.")
-        });
-    })();
+(function(){
+    const baseURL  = 'http://localhost:8086';
+
+    async function testAPIs(){
+        try{
+            // display all trips
+            let list = await callAPI('GET', '/api/trips', null, null )
+            console.log('\n---------Return trip list------------\n')
+            console.log(list)
+        } catch(err){
+            console.error(`Error: ${err}}`)
+        }
+        
+    };
+
+    async function callAPI(method, uri, params, body){
+        try{
+            const response = await fetch(baseURL + uri, {
+                method: method,
+            });
+            return response.json();
+        }catch(err){
+            console.error(`Error: ${err}`);
+            
+        }
+        
+    }
+
+    document.querySelector('#test-button').addEventListener("click", ()=> {
+        console.log("Test API button been clicked.")
+        let upload = document.querySelector('input[type="file"]')
+        if (upload.value){
+            testAPIs();
+        } else {
+            alert('Select an image file to test the REST APIs')
+        }
+    });
+})();
 
 

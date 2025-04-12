9ff3dcbcd809e107babd63caea6f8736deca727a
Yu-Ching Huang <yu.ching.huang@outlook.com>
2025-04-12 16:47:24 -0400

    edit api.js file

---
+++ public/js/api.js
@@ -0,0 +1,10 @@
+
+    (function(){
+        const URL  = 'localhost:8086';
+    
+        document.querySelector('#test-button').addEventListener("click", ()=> {
+            console.log("Test API button been clicked.")
+        });
+    })();
+
+

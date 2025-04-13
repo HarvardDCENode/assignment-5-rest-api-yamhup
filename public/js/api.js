5d0b88e134b90719dafd4d033f190c862ef6c958
Yu-Ching Huang <yu.ching.huang@outlook.com>
2025-04-13 12:01:56 -0400

    add POST method to callAPI()

--- public/js/api.js
+++ public/js/api.js
@@ -1,13 +1,32 @@
+// Reference from week9 -09.1-express-rest-api "api-async-await.js" file
 
 (function(){
     const baseURL  = 'http://localhost:8086';
 
     async function testAPIs(){
         try{
-            // display all trips
-            let list = await callAPI('GET', '/api/trips', null, null )
-            console.log('\n---------Return trip list------------\n')
-            console.log(list)
+            // displays all trips
+            let list = await callAPI('GET', '/api/trips', null, null );
+            console.log('\n---------Return trip list------------\n');
+            console.log(list);
+
+            //creates a trip itinerary and uploads an image
+            let newImage = document.querySelector('input[type="file"]');
+            //creates a new FormData object
+            let data = new FormData()
+            data.append('image', newImage.files[0]);
+            data.append('username', 'Testing API username')
+            data.append('title', 'Testing API title');
+            data.append('country', 'Testing API country');
+            data.append('city', 'Testing API city');
+            data.append('content', 'This is an AJAX API test');
+
+            let newTrip = await callAPI('POST', '/api/trips', null, data)
+            tripId = newTrip._id;
+            console.log('\n---------Create a new trip ------------\n');
+            console.log(newTrip)
+
+
         } catch(err){
             console.error(`Error: ${err}}`)
         }
@@ -18,6 +37,7 @@
         try{
             const response = await fetch(baseURL + uri, {
                 method: method,
+                ...(method == 'POST' ? {body:body}: {})
             });
             return response.json();
         }catch(err){
